bce0ea5ae952ecf62a32e170a20660932014d78e
Babar Ali <babardeveloper2@gmail.com>
2025-03-02 03:21:49 +0500

    create page & routes

--- blogging-website/src/App.jsx
+++ blogging-website/src/App.jsx
@@ -1,5 +1,24 @@
-const App = () => {
-  return <div className="text-blue-400 underline text-3xl">Hello World!</div>;
-};
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Error from "./pages/Error";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
