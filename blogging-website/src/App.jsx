595d1475818a3a588b464edc46ca5981da189aeb
Babar Ali <babardeveloper2@gmail.com>
2025-03-02 07:06:19 +0500

    create a header section

--- blogging-website/src/App.jsx
+++ blogging-website/src/App.jsx
@@ -6,10 +6,12 @@ import Dashboard from "./pages/Dashboard";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Error from "./pages/Error";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- blogging-website/src/pages/Error.jsx
+++ blogging-website/src/pages/Error.jsx
@@ -1,7 +1,18 @@
-
+import { Alert, Button } from "flowbite-react";
+import { HiEye, HiInformationCircle } from "react-icons/hi";
+import Projects from "./Projects";
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <Alert
+      additionalContent={<Projects />}
+      color="success"
+      icon={HiInformationCircle}
+      onDismiss={() => alert("Alert dismissed!")}
+      rounded
+    >
+      <span className="font-medium">Info alert!</span> Change a few things up
+      and try submitting again.
+    </Alert>
+  );
 }
