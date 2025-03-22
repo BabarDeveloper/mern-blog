91be13e992b1427a8ea55ea5c4cc9b907d19cba1
Babar Ali <babardeveloper2@gmail.com>
2025-03-23 01:14:07 +0500

    show user posts inside dashboard

--- blogging-website/src/main.jsx
+++ blogging-website/src/main.jsx
@@ -1,4 +1,4 @@
-import { createRoot } from "react-dom/client.js";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { store, persistor } from "./redux/store.js";
