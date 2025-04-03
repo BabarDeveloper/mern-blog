7dea1ebf59b01f496db6923447c9fed85926e68f
Babar Ali <babardeveloper2@gmail.com>
2025-04-03 20:49:25 +0500

    complete search page

--- blogging-website/src/App.jsx
+++ blogging-website/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./components/Search";
 
 export default function App() {
   return (
@@ -34,6 +35,7 @@ export default function App() {
         </Route>
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/*" element={<Error />} />
       </Routes>
       <FooterCom />
